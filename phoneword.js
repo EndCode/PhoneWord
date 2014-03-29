ee8fef042b407905a0e361b01223c859f030e98a
Luke West <l.s.west88@gmail.com>
2014-03-29 01:17:25 -0400

    Made silly mistakes, and called some wonky files trusted files. Changed that.

--- phoneword.js
+++ phoneword.js
@@ -1,16 +1,18 @@
-function spaceNotTaken(index, length, takenList){
-	//Determines whether indices of the number are used or not.
+function spaceNotTaken(index, length, takenList, newTakenList){
+	//Determines whether indices of the number are used or not, and assings newTakenList to be the new takenList.
 	for (var i = index; i < index + length;  i++){
 		if (takenList.indexOf(i) > -1){
 			return false;
 		}
 	}
 
+	//newTakenList = takenList.slice(0);
 	for (var i = index; i < index + length;  i++){
 		takenList.push(i);
 	}
 	return true
 }
+
 
 
 
@@ -23,7 +25,7 @@ function makeSubsets(text, size){
 	var subsetList = [];
 
 	for (var index = 0; index <= usableLength; index++){
-		var subset = text.slice(index, index + size);
+		subset = text.slice(index, index + size);
 		//subsetList.push(subset);
 
 		if (keyList.indexOf(subset) > -1){
@@ -40,7 +42,7 @@ function makeSubsets(text, size){
 
 function makeAllSubsets(text){
 	//creates all subsets of any size from the given text
-	var subsetList = [];
+	subsetList = [];
 
 	for (var size = 1; size <= text.length; size++){
 		subsetList = subsetList.concat(makeSubsets(text, size));
@@ -52,50 +54,42 @@ function makeAllSubsets(text){
 
 
 
-
 function convertNum(subsets, number, numList, takenList){
+	//ERROR: takenList can't change within the recur, which it does.
+	//ERROR: Taking only parts of words.
 	
 	var tempTakenList = takenList.slice(0);
-	console.log (["Start, the subsets are ", subsets, ", the number is ", number, ", the numlist is ", numList, ", and the takenList is ", takenList].join(""));
+	console.log (["Start", subsets, number, numList, takenList]);
 
 	for (var i = 0; i < subsets.length; i++){ //for each subset
 		takenList = tempTakenList.slice(0);
-		console.log(["Subset loop activated, and the current number is : ", number].join(""));
-		console.log(["Iteration: ", i].join(""));
-		console.log(["Indices Taken: ", takenList].join(""));
-		console.log(["tempTakenList, which shouldn't change: ", tempTakenList].join(""));
-		var pair = subsets[i];
-		var num = pair[0];
-		var index = pair[1];
-		console.log(["The subset is ", num, ", and the index is ", index].join(""));
+		console.log(["Subset loop activated, and the current number is :", number]);
+		console.log(["Iteration: ", i]);
+		console.log(["Indices Taken: ", takenList]);
+		console.log(["tempTakenList, which shouldn't change: ", tempTakenList]);
+		pair = subsets[i];
+		num = pair[0];
+		index = pair[1];
+		console.log(["The subset is ", num, "and the index is ", index]);
 
 		if (spaceNotTaken(index, num.length, takenList)){ //if the space for the words connected to the num has not been used
 			console.log("Space not taken");
 
 			for (var x = 0; x < numWordDict[num].length; x++){ //for each word connected to num
-				var word = numWordDict[num][x]
-				console.log(["numWord loop activated, the current number is ", number, ", the current subset is ", num, ", and the current word is ", word].join(""));
-				var newCombo = number.slice(0, index) + word + number.slice(index + word.length);
-				console.log(["The newCombo is: ", newCombo].join(""));
-
-					if (newCombo == "RAPME"){
-						alert("RAPME");
-					}
+				console.log("numWord loop activated");
+				word = numWordDict[num][x]
+				newCombo = number.slice(0, index) + word + number.slice(index + word.length);
+				console.log(["The newCombo is: ", newCombo]);
 
 				if (numList.indexOf(newCombo) < 0){
 					console.log("newCombo not in numList");
 					numList.push(newCombo);
 					convertNum(subsets, newCombo, numList, takenList.slice(0));
 				}
-
 			}
 		}
 	}
 }
-
-
-
-
 
 function phoneWord(){
 	var box = document.getElementById("outputBox");
@@ -107,10 +101,11 @@ function phoneWord(){
 	var numList = [];
 	var subsets = makeAllSubsets(phoneNumber);
 	var takenList = [];
+	var newTakenList = [];
 
 
 	for (var i = 0; i < phoneNumber.length; i++){
-		var character = phoneNumber.charAt(i);
+		character = phoneNumber.charAt(i);
 
 		if (!(character in nums)){
 			alert("Please enter a valid phone number, using only numbers.");
@@ -118,14 +113,10 @@ function phoneWord(){
 		}
 	}
 
-	convertNum(subsets, phoneNumber, numList, takenList);
-	convertToDivs(numList, box);
+	convertNum(subsets, phoneNumber, numList, takenList, newTakenList);
 	console.log(numList);
 	var newNumsString = numList.join("\n");
 	text.innerHTML = newNumsString;
 	box.style.visibility = "visible";
 	return;
 }
-
-//TODO: Define function that divides the resulting numWords into number parts and word parts, both with their own classes, to assign colors to.
-
